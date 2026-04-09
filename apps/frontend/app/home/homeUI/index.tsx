ac05f8d5919deb44e981c9fb3912588a94d069d3
HanJeongWoo <87626378+blackeichi@users.noreply.github.com>
2026-04-09 21:58:05 +0900

    feat: AI 행동지침 개선 및 Home UI 개선

--- apps/frontend/app/home/homeUI/index.tsx
+++ apps/frontend/app/home/homeUI/index.tsx
@@ -10,11 +10,11 @@ export default function HomeUI({ initialData }: HomeUIProps) {
     "오늘도 충분히 잘하고 있어요! \n 천천히 가도 괜찮아요. 😉";
 
   return (
-    <div className="w-full min-h-screen max-w-[800px]">
+    <div className="w-full min-h-screen max-w-200">
       <div className="mx-auto max-w-4xl space-y-6">
         {/* 한마디 응원 카드 */}
-        <section className="rounded-lg bg-white p-6 shadow-lg shadow-stone-500 ">
-          <div className="mb-4 flex items-center gap-3">
+        <section className="rounded-lg bg-white shadow-lg shadow-stone-500 overflow-hidden">
+          <div className="flex items-center gap-3 p-4 py-6 bg-stone-700 text-white">
             <div className="flex sm:h-10 sm:w-10 h-8 w-8 items-center justify-center rounded-full bg-blue-100 sm:text-xl text-lg border border-stone-200">
               😃
             </div>
@@ -23,13 +23,15 @@ export default function HomeUI({ initialData }: HomeUIProps) {
             </div>
           </div>
 
-          <div className="rounded-lg bg-blue-50 px-5 py-6 border border-stone-200">
-            <p className="leading-7 whitespace-pre-line">{message}</p>
+          <div className="bg-white p-6 sm:p-8 border border-stone-200 flex flex-col gap-6 sm:gap-6 items-center text-sm sm:text-base">
+            <p className="leading-4 sm:leading-6 whitespace-pre-line text-center">
+              {message}
+            </p>
           </div>
         </section>
 
         {/* 차트 영역 자리 */}
-        <section className="rounded-lg bg-white p-6 shadow-lg shadow-stone-500 ">
+        <section className="rounded-lg bg-white p-6 shadow-lg shadow-stone-500">
           <h3 className="sm:text-lg text-base font-semibold text-gray-900 mb-4">
             주간 통계
           </h3>
