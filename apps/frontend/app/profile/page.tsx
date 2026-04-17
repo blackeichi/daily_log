05fca6298682f951f2d9302e910e47ef691dc5b8
HanJeongWoo <87626378+blackeichi@users.noreply.github.com>
2026-04-18 07:30:10 +0900

    refactor: 프로필 페이지 리팩토링

--- apps/frontend/app/profile/page.tsx
+++ apps/frontend/app/profile/page.tsx
@@ -2,9 +2,12 @@ import { Metadata } from "next";
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
 
-const ProfileUI = dynamic(() => import("./profileUI"), {
-  loading: () => <div className="w-full h-full bg-stone-100" />,
-});
+const ProfileUI = dynamic(
+  () => import("@/features/profile/components/ProfileUI"),
+  {
+    loading: () => <div className="w-full h-full bg-stone-100" />,
+  },
+);
 
 export const metadata: Metadata = {
   title: "프로필",
